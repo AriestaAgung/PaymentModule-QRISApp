8bd290a6968b3ff737a4945fc20b26dc0835c227
Ariesta <ariesta.work@gmail.com>
2024-03-13 09:21:50 +0700

    Update Dependency

--- Package.swift
+++ Package.swift
@@ -11,11 +11,20 @@ let package = Package(
             name: "PaymentModule",
             targets: ["PaymentModule"]),
     ],
+    dependencies: [
+        .package(path: "../BalanceCore"),
+        .package(path: "../TransactionCore"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PaymentModule"),
+            name: "PaymentModule",
+            dependencies: [
+                "BalanceCore",
+                "TransactionCore"
+            ]
+        ),
         .testTarget(
             name: "PaymentModuleTests",
             dependencies: ["PaymentModule"]),
